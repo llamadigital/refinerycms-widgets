895149e410514a1cf407afa80391aeea72047207
Nick <nick@llamadigital.net>
2014-01-23 12:23:29 +0000

    remove preview and continue editing links

--- app/overrides/refinery/admin/pages/_form.rb
+++ app/overrides/refinery/admin/pages/_form.rb
@@ -2,3 +2,8 @@ Deface::Override.new(virtual_path: 'refinery/admin/pages/_form',
                     name: 'insert_template_id_field_to_pages_admin_form',
                     insert_after: "erb[loud]:contains('form_for')",
                     text: "<%= f.hidden_field :template_id -%>")
+
+Deface::Override.new(virtual_path: 'refinery/admin/pages/_form',
+                    name: 'remove_preview_and_continue_links',
+                    replace: "erb[loud]:contains('admin/form_actions')",
+                    partial: 'refinery/admin/pages/form_actions')
